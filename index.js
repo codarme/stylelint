3ec0b42adb0244212a8632bad6460986c157354b
Bruno Bertolini <dev@brunobertolini.com>
2019-11-10 13:57:22 -0300

    feat: add eslint and prettier basic config

---
+++ index.js
@@ -0,0 +1,44 @@
+const prettier = require('./prettier.config.js')
+
+module.exports = {
+  configs: {
+    react: {
+      parser: 'babel-eslint',
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+      ],
+
+      env: {
+        node: true,
+      },
+      rules: {
+        'comma-dangle': [
+          'error',
+          {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'never',
+          },
+        ],
+        'react/prop-types': [2, { skipUndeclared: true }],
+        'prettier/prettier': ['error', prettier],
+      },
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      settings: {
+        react: {
+          version: 'detect',
+        },
+      },
+    },
+  },
+}
